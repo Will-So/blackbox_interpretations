411443bbb61ae8e2d5c0d5ee77b6417045c9f894
will <wil.sorenson@gmail.com>
2017-12-25 21:10:58 -0500

    tests passing

--- tests/test_best_tree.py
+++ tests/test_best_tree.py
@@ -2,7 +2,7 @@ from blackbox_interpretations.best_tree import get_best_tree, visualize_tree
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
-
+from graphviz.files import Source
 X, Y = make_classification(n_features=2, n_redundant=0, n_informative=2,
                              n_clusters_per_class=1, random_state=555)
 
@@ -16,4 +16,4 @@ def test_get_best_tree():
 
     graph = visualize_tree(best_tree, ['feature_1', 'feature_2'], ['0', '1'])
 
-    assert type(graph) == 'graphviz.files.Source'
+    assert type(graph) == Source
